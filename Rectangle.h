1ce7900b7f43f16c9e7eca08f91ac1e883984310
varshamadipadga <61565374+varshamadipadga@users.noreply.github.com>
2024-01-28 23:32:58 -0600

    Update Rectangle.h

--- Rectangle.h
+++ Rectangle.h
@@ -1,1 +1,19 @@
 /**Rectangle.h Code*/
+#ifndef RECTANGLE_
+#define RECTANGLE_
+
+#include "RectangleInterface.h"
+
+class Rectangle : public RectangleInterface
+{
+public:
+	Rectangle();
+	bool set( double newLength, double newWidth );
+  double getLength() const;
+  double getWidth() const;
+  double getArea() const;
+private:
+	double width;
+  double length;
+};
+#endif
