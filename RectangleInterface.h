41edeef7fe001a7ccfe69a456a1fb12a738b992f
varshamadipadga <61565374+varshamadipadga@users.noreply.github.com>
2024-01-28 23:29:02 -0600

    Update RectangleInterface.h

--- RectangleInterface.h
+++ RectangleInterface.h
@@ -1,1 +1,13 @@
 /** RectangleInterface Code*/
+#ifndef RECTANGLE_INTERFACE_
+#define RECTANGLE_INTERFACE_
+
+class Rectangle
+{
+public:
+ 
+	virtual double getLength() const = 0;
+  virtual double getWidth() const = 0;
+  virtual double getArea() const = 0;
+};
+#endif
