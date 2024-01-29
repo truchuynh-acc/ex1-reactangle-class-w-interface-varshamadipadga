43fa1bc574dae634d9a9a7124aa0cfaa813e4c26
varshamadipadga <61565374+varshamadipadga@users.noreply.github.com>
2024-01-28 23:42:54 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,9 +1,27 @@
 /**Driver Code*/
 
 /**
-Programmer: 
+Programmer: Varsha Madipadga
 Project number: 1
 Project Desc: Quadratic Expression class
 Course: COSC-2436-007 (92905) III Data Structures
 Date: 01/28/2024
 */
+
+#include <iostream>
+#include "Rectangle.h"
+using namespace std;
+
+int main()
+{
+	Rectangle theRectangle;
+	
+	cout << "Intital state of the rectangle" << endl;
+	cout << "    length = " << theRectangle.getLength() << endl;
+  cout << "    width = " << theRectangle.getWidth() << endl;
+	cout << "    area   = " << theRectangle.getArea() << endl << endl;
+	
+	
+	
+	return 0;
+}
