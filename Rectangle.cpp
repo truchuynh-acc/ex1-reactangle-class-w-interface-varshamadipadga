508647e5bb823efdaf215ec51bcc7e6bcf4d7d9c
varshamadipadga <61565374+varshamadipadga@users.noreply.github.com>
2024-01-28 23:37:38 -0600

    Update Rectangle.cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,1 +1,30 @@
 /**Rectangle.cpp Code*/
+#include "Rectangle.h"
+
+Rectangle::Rectangle()
+{
+	length = width = 1;
+}
+
+bool Rectangle::set( double newLength, double newWidth )
+{
+	if (newLength > 0 && newWidth > 0)
+	{
+    length = newLength;
+    width = newWidth;
+    return true
+   
+	}
+	else
+    return false
+}
+
+double Rectangle::getWidth() const
+{
+	return width;
+}
+
+double Rectangle::getArea() const
+{
+	return length * width;
+}
